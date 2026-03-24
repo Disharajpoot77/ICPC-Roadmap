e4ad8681dfc2729b8dcc6d0bff6df329f263d149
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-24 14:57:39 +0530

    stones

---
+++ colourful_stones.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string s,t;
+	cin>>s>>t;
+	int count=0;
+	int j=0;
+	for(int i=0; i<t.size(); i++ ){
+
+		if(s[count]==t[i]){
+			count++;
+		}
+		 
+	}
+
+	cout<<++count<<endl;
+
+}
