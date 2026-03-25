5bcde675b3f9c983646dec27a548ce8710f6ac7a
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-25 14:59:40 +0530

    word

---
+++ word_Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string s;
+	cin>>s;
+	 if(s[0]>='A' && s[0]<='Z'){
+		cout<<s<<endl;
+	}
+	else{
+       char t=s[0]-32;
+		 s[0]=t;
+		 cout<<s<<endl;
+	}
+	
+	return 0;
+
+}
