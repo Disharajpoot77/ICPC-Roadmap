0b888b45f182d76d485e649c1ff35f7adbadcbb4
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-31 21:26:51 +0530

    new

---
+++ Night.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+    char past_char = 'a';
+    int ans = 0;
+    // string s;
+    // cin >> s;
+ 
+    for (int i = 0; i < s.length(); i++) {
+ 
+        int diff = abs(past_char - s[i]);
+ 
+        if(diff > 13)
+            ans += (26 - diff);
+        else
+            ans += diff;
+ 
+        past_char = s[i];
+    }
+ 
+    cout << ans;
+    
+    return 0;
+}
