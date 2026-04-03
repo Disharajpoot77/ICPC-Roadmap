196e91140cfbc12d8b69c6fd4a31a8a722d9a8cd
Disha rajpoot <rajpootdisha42@gmail.com>
2026-04-03 20:42:08 +0530

    done

--- snow_footprints.cpp
+++ snow_footprints.cpp
@@ -14,32 +14,39 @@ int main(){
 	else if(s.size()==3 && first=='L'){
             cout<<"2"<<" "<<"1";
 	}
+	else{
      
-	int start=0;
+		int start=0;
 	int end=0;
 	for(int i=0; i<s.size(); i++){
-		if(s[i]=='R'){
-           if(start==0){
-			   start=i+1;
-		   }
 
-		   else{
-			   end=i+2;
-		   }
+	      if(s[i]=='L'){
+             end=i;
+	         for(int j=i; s[j]!='.'; j++){
+                  start=j+1;
+			   }
+			   break;
 		}
-       else if(s[i]=='L'){
-		   if(end==0){
-			   end=i;
-		   }
-
-		   else{
-			   start=i+1;
-		   }	
+		else if(s[i]=='R'){
+           start=i+1;
+	         for(int j=i; s[j]!='.'; j++){
+				if(s[j]=='L'){
+		  	     start=j;
+			     end=j;
+			     break; 	
+	        	}
+          end=j+2;
+	  }
+			   break;
 		}
 		
 		
 	}
 	cout<<start<<" "<<end;
+
+	}
+     
+	
 
 	return 0;
 }
