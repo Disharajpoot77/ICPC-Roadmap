59766205ebf88d72815f4df81074dbf3dc119f38
Disha rajpoot <rajpootdisha42@gmail.com>
2026-04-04 14:44:45 +0530

    string

---
+++ String_task.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+    string update="";
+    
+	for(int i=0; i<s.size(); i++){
+
+		if(s[i]!='a'&& s[i]!='e' && s[i]!='i'&&s[i]!='o'&&s[i]!='u'&&s[i]!='y'&& s[i]!='A' && s[i]!='E' && s[i]!='I' && s[i]!='O' && s[i]!='U'&&s[i]!='Y'){
+		  update+='.';
+		  update+=s[i];
+	}
+	
+
+}
+
+ for (char &ch : update) {
+   ch = tolower(ch);
+    }
+cout<<update;
+return 0;
+}
