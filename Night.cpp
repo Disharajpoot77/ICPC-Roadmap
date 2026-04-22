695d0216f37249258a22efe1ba7d20038cf571a8
Disha rajpoot <rajpootdisha42@gmail.com>
2026-04-22 13:57:54 +0530

    night

--- Night.cpp
+++ Night.cpp
@@ -9,7 +9,7 @@ int main() {
     // string s;
     // cin >> s;
  
-     for (int i = 0; i < s.length(); i++) {
+     for(int i = 0; i < s.length(); i++) {
  
          int diff = abs(past_char - s[i]);
  
